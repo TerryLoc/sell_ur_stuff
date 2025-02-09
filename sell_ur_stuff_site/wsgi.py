560680009bb8f385dd00758278d338ff4b114516
TerryLoc <terryloughran@gmail.com>
2025-02-09 09:16:17 +0000

    update migrate

--- sell_ur_stuff_site/wsgi.py
+++ sell_ur_stuff_site/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sell_ur_stuff_site.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "/sell_ur_stuff/sell_ur_stuff_site.settings",
+)
 
 application = get_wsgi_application()
