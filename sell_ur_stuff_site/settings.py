1595781430e421083c3efe92abe4cd91af027f29
TerryLoc <terryloughran@gmail.com>
2025-02-09 10:38:18 +0000

    Fix CSRF_TRUSTED_ORIGINS

--- sell_ur_stuff_site/settings.py
+++ sell_ur_stuff_site/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-2%ek4)y646i#h=0#u63&ygwlq#^093z3kw08=*c*+=&=7(go!@
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    os.environ.get("HEROKU_APP_NAME", "sell_ur_stuff.herokuapp.com"),
+    ".herokuapp.com",
     "localhost",
     "127.0.0.1",
 ]
