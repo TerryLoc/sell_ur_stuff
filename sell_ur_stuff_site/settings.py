851085b3d7fad11c153faf2069213f21da910693
TerryLoc <terryloughran@gmail.com>
2025-02-09 15:13:24 +0000

    Update: homepage title and subtitle

--- sell_ur_stuff_site/settings.py
+++ sell_ur_stuff_site/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-2%ek4)y646i#h=0#u63&ygwlq#^093z3kw08=*c*+=&=7(go!@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".herokuapp.com",
