b4216a1e35e9348247a64bf2054b6dd082b621b8
TerryLoc <terryloughran@gmail.com>
2025-02-23 09:13:31 +0000

    feat: add profile editing functionality

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile, Purchase, Notification
 from sales.models import Sale
+from .forms import UserProfileForm  # We’ll create this
 
 
 @login_required
@@ -21,3 +22,16 @@ def profile_view(request):
             "notifications": notifications,
         },
     )
+
+
+@login_required
+def profile_edit(request):
+    profile = request.user.profile
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+    else:
+        form = UserProfileForm(instance=profile)
+    return render(request, "profiles/profile_form.html", {"form": form})
