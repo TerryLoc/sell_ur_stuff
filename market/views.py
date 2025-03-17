39aec24c7c54cfed97e128712ef95a92cae22e9e
TerryLoc <terryloughran@gmail.com>
2025-03-17 18:06:34 +0000

    feat: update market list view order by date

--- market/views.py
+++ market/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render
 from sales.models import Sale
 
 
 def market_list(request):
     """
-    The list of all available products and display them in the market
+    The list of all available products and display them in the market page.
     """
-    products = Sale.objects.filter(status="available")  # Only show available items
+    products = Sale.objects.filter(status="available").order_by("-created_at")
     return render(request, "market/market.html", {"products": products})
