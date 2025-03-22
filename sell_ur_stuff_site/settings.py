970b64bcdb737098d041c7c96a9c66953ecb2de6
TerryLoc <terryloughran@gmail.com>
2025-03-22 13:10:13 +0000

    feat: enable dynamic debug using variable

--- sell_ur_stuff_site/settings.py
+++ sell_ur_stuff_site/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# S0 we don't run with debug turned on in production!
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
     ".herokuapp.com",
