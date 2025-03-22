6ef8366e586f613ad764c17d1a8c333f7dea3356
TerryLoc <terryloughran@gmail.com>
2025-03-22 18:09:19 +0000

    feat: add cloudinary configuration & requirements

--- sell_ur_stuff_site/settings.py
+++ sell_ur_stuff_site/settings.py
@@ -31,6 +31,12 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+}
+
 # S0 we don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False) == True
 
@@ -54,6 +60,9 @@ INSTALLED_APPS = [
     "profiles",
     "contact",
     "widget_tweaks",
+    # Third-party apps for image storage
+    "cloudinary_storage",
+    "cloudinary",
     # Django apps
     "django.contrib.admin",
     "django.contrib.auth",
@@ -127,19 +136,22 @@ LOGOUT_REDIRECT_URL = "/"  # Redirect after logout
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# Database configuration for Heroku
+# Database configuration
 if "DATABASE_URL" in os.environ:
-    # Use PostgreSQL in production
     DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+    # Media files configuration for production
+    DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 else:
-    # Use SQLite locally
+    # Local database
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
-
+    # Keep local media settings for development
+    MEDIA_URL = "/media/"
+    MEDIA_ROOT = BASE_DIR / "media"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -175,9 +187,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# Media files (user-uploaded files)
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
