a90bda7dfa6ff8ad284ee0b72a2185ae109fee7e
TerryLoc <terryloughran@gmail.com>
2025-03-23 16:15:57 +0000

    Feat: customize User admin, update UserProfile form

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import UserProfile, Purchase, Notification
 
-# Register your models here.
+
+# Define an inline admin descriptor for UserProfile
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = "User Profiles"
+
+
+# Define a new User admin
+class CustomUserAdmin(UserAdmin):
+    # Customize fieldsets without duplicating fields
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal info",
+            {"fields": ("first_name", "last_name", "email")},
+        ),  # Already includes first_name and last_name
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    # Display these fields in the list view
+    list_display = ("username", "email", "first_name", "last_name", "is_staff")
+    # Add the inline
+    inlines = (UserProfileInline,)
+
+
+# Unregister the default User admin and register the custom one
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+# Register other models
+admin.site.register(UserProfile)
+admin.site.register(Purchase)
+admin.site.register(Notification)
--- profiles/forms.py
+++ profiles/forms.py
@@ -1,8 +1,37 @@
 from django import forms
 from .models import UserProfile
+from django.contrib.auth.models import User
 
 
 class UserProfileForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=30, required=False, label="First Name")
+    last_name = forms.CharField(max_length=30, required=False, label="Last Name")
+
     class Meta:
         model = UserProfile
-        fields = ["profile_picture", "location", "bio", "phone_number", "address"]
+        fields = [
+            "first_name",
+            "last_name",
+            "profile_picture",
+            "location",
+            "bio",
+            "phone_number",
+            "address",
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Prepopulate first_name and last_name from the User model
+        if self.instance and self.instance.user:
+            self.fields["first_name"].initial = self.instance.user.first_name
+            self.fields["last_name"].initial = self.instance.user.last_name
+
+    def save(self, commit=True):
+        profile = super().save(commit=False)
+        user = profile.user
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        if commit:
+            user.save()
+            profile.save()
+        return profile
