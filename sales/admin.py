e5860f07b6773f697f4546759cfa1645fcaa47f7
TerryLoc <terryloughran@gmail.com>
2025-05-18 11:59:18 +0100

    Chore: Register models in admin.py for Sale, Offer, and Purchase

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Sale, Offer, Purchase  # Import Purchase model
 
-# Register your models here.
+admin.site.register(Sale)
+admin.site.register(Offer)
+admin.site.register(Purchase)  # Register Purchase model
