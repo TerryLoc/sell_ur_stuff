adb0686e03226ae9f1234b3e1ac56c0e12b9c596
TerryLoc <terryloughran@gmail.com>
2025-05-18 12:40:49 +0100

    Chore: Remove Purchase model from admin.py / sales

--- sales/admin.py
+++ sales/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Sale, Offer, Purchase  # Import Purchase model
+from .models import Sale, Offer
 
 admin.site.register(Sale)
 admin.site.register(Offer)
-admin.site.register(Purchase)  # Register Purchase model
